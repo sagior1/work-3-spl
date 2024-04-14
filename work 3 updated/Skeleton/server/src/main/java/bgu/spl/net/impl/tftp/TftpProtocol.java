e6ee89512c48d695ebf28ed670853bd910ed573a
sagior1 <157025263+sagior1@users.noreply.github.com>
2024-04-14 13:33:14 +0300

    Update TftpProtocol.java

--- work 3 updated/Skeleton/server/src/main/java/bgu/spl/net/impl/tftp/TftpProtocol.java
+++ work 3 updated/Skeleton/server/src/main/java/bgu/spl/net/impl/tftp/TftpProtocol.java
@@ -1,10 +1,11 @@
 package bgu.spl.net.impl.tftp;
 
 import bgu.spl.net.api.BidiMessagingProtocol;
+import bgu.spl.net.impl.tftp.TftpEncoderDecoder.Opcode;
 import bgu.spl.net.srv.Connections;
 
 public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
-
+    private boolean shouldTerminate = false;
     @Override
     public void start(int connectionId, Connections<byte[]> connections) {
         // TODO implement this
@@ -13,16 +14,38 @@ public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
 
     @Override
     public void process(byte[] message) {
-        // TODO implement this
-        throw new UnsupportedOperationException("Unimplemented method 'process'");
+        TftpEncoderDecoder.Opcode opcode = extractOpcode(message);
+        switch (opcode) {
+            case RRQ:
+                //handleReadRequest(message);
+                break;
+            case WRQ:
+                //handleWriteRequest(message);
+                break;
+            case DATA:
+                //handleData(message);
+                break;
+            case ACK:
+                //handleAck(message);
+                break;
+            case ERROR:
+                //handleError(message);
+                break;
+            // Add cases for other opcodes
+            default:
+                break;//אולי לעשות פה משהו אחר
+        }
     }
 
     @Override
     public boolean shouldTerminate() {
-        // TODO implement this
-        throw new UnsupportedOperationException("Unimplemented method 'shouldTerminate'");
+        return shouldTerminate;
     } 
-
+    private TftpEncoderDecoder.Opcode extractOpcode(byte[] data) {
+        // Extract opcode assuming data is not null and has at least two bytes
+        int opCodeNum = ((data[0] & 0xFF) << 8) | (data[1] & 0xFF);
+        return TftpEncoderDecoder.Opcode.fromU16(opCodeNum);
+    }
 
     
 }
