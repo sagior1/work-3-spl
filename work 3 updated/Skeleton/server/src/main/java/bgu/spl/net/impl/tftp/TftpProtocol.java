0258bef5bb9ca4ac78e4b1c707ea26f720ef1722
sagior1 <157025263+sagior1@users.noreply.github.com>
2024-04-15 12:56:47 +0300

    Update TftpProtocol.java

--- work 3 updated/Skeleton/server/src/main/java/bgu/spl/net/impl/tftp/TftpProtocol.java
+++ work 3 updated/Skeleton/server/src/main/java/bgu/spl/net/impl/tftp/TftpProtocol.java
@@ -1,20 +1,34 @@
 package bgu.spl.net.impl.tftp;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 import bgu.spl.net.api.BidiMessagingProtocol;
 import bgu.spl.net.impl.tftp.TftpEncoderDecoder.Opcode;
 import bgu.spl.net.srv.Connections;
 import bgu.spl.net.srv.ConnectionsImpl;
+import java.util.LinkedList;
+
 
 public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
     private boolean shouldTerminate = false;
     int connectionId;
     private ConnectionsImpl<byte[]> connections;
+    int blocknum;
+    LinkedList<byte[]> dataToSend;
+    private Path serverPath;
 
 
     @Override
     public void start(int connectionId, Connections<byte[]> connections) {
         this.connectionId=connectionId;
         this.connections=(ConnectionsImpl<byte[]>)connections;
+        blocknum=0;
+        serverPath=Paths.get("").toAbsolutePath().resolve("Flies");
     }
 
     @Override
@@ -36,6 +50,7 @@ public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
                 disc();
                 break;
             case DATA:
+                data(message);
                 break;
             case BCAST:
                 break;
@@ -79,6 +94,30 @@ public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
             //TODO ack
         }
     }
+    private void ack(int num){
+        short a = (short) num;
+        connections.send(connectionId, new byte []{0,4,(byte)(a >> 8) , (byte)( a & 0xff )});
+    }
 
-    
+    private void data(byte[] message){
+        short length = (short) (((short) message[0] ) << 8 | ((short) message[1] ));//TODO delete
+        short blockNum = (short) (((short) message[2] ) << 8 | ((short) message[3] ));//TODO delete
+        byte[] data = Arrays.copyOfRange(message, 4, message.length);//TODO delete
+        byte[] dataArray = Arrays.copyOfRange(message, 6, message.length);
+        dataToSend.add(dataArray);
+        blockNum++;
+        ack(blockNum);
+        if(dataArray.length<512){
+            File uploadAddress = new File(filesPath + "/" + this.fileNameToWrite);
+            try (FileOutputStream out = new FileOutputStream(addressToWrite, true)) {
+                byte[] finalData = new byte[dataToWrite.size()];
+                for (int i=0; i<finalData.length; i++){
+                    finalData[i] = dataToWrite.removeFirst();
+                }
+                uploadAddress.createNewFile();
+                out.write(finalData, 0, finalData.length);
+                bcast(false, this.fileNameToWrite);
+            } catch (IOException e) {
+        }
+    }
 }
