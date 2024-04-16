92cc8e2ddbf77c628debadcaebc12388feb7dbe0
sagior1 <157025263+sagior1@users.noreply.github.com>
2024-04-16 15:06:06 +0300

    Update TftpServer.java

--- work 3 updated/Skeleton/server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
+++ work 3 updated/Skeleton/server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
@@ -1,5 +1,21 @@
 package bgu.spl.net.impl.tftp;
 
+import bgu.spl.net.srv.Server;
+
 public class TftpServer {
-    //TODO: Implement this
+    public static void main(String[] args) {
+        if(args.length>1){
+            return;
+        }
+        int port = 7777;
+        if(args.length==1){
+         port = Integer.parseInt(args[0]);
+        }
+        Server.threadPerClient(  
+                port, //port
+                (() -> new TftpProtocol()), //protocol factory
+                TftpEncoderDecoder::new //message encoder decoder factory
+        ).serve();
+        
+    }
 }
